809e0ec65d0cad4e1f287e5f4f0188c8fdf2e11e
Igor Vilkov <igorvilkov404@gmail.com>
2024-08-13 21:39:43 +0300

    initial commit

---
+++ src/test/java/io/igorv404/library/LibraryApplicationTests.java
@@ -0,0 +1,11 @@
+package io.igorv404.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
