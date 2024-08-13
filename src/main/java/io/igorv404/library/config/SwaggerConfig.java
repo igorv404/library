3790e533d1fff68c83e1a78eccb1c134a8e7abb4
Igor Vilkov <igorvilkov404@gmail.com>
2024-08-13 21:54:59 +0300

    added swagger

---
+++ src/main/java/io/igorv404/library/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package io.igorv404.library.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+  @Bean
+  public OpenAPI customOpenAPI() {
+    Contact contact = new Contact()
+        .name("Igor Vilkov")
+        .email("igorvilkov404@gmail.com")
+        .url("https://www.linkedin.com/in/igor-vilkov/");
+    Info info = new Info()
+        .title("Library")
+        .version("0.2.0")
+        .description("System for managing library")
+        .contact(contact);
+    return new OpenAPI().info(info);
+  }
+}
