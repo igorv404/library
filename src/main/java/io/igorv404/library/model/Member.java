8c581d47a2a21e5d0c7a0ee38c64b06a8aeb9474
Igor Vilkov <igorvilkov404@gmail.com>
2024-08-14 11:57:12 +0300

    created member entity

---
+++ src/main/java/io/igorv404/library/model/Member.java
@@ -0,0 +1,36 @@
+package io.igorv404.library.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(nullable = false)
+  @NotBlank
+  private String name;
+
+  @Column(nullable = false)
+  private final LocalDate membershipDate = LocalDate.now();
+
+  @OneToMany
+  private List<Book> borrowedBooks;
+}
